816f53a7ffa3391861452ced5f4700ae0073534d
ENNOA <evajunkie@gmail.com>
2024-02-29 20:25:36 -0500

    initial commit

---
+++ src/JavaAPI/AIAssist/com/transcribe/AIAssist/sfcder.java
@@ -0,0 +1,10 @@
+package com.transcribe.AIAssist;
+
+import java.io.File;
+
+public class sfcder {
+	public static void main(String[] args) {
+		File file = new File("C:\\Users\\evaju\\Documents\\GitHub\\AsemblyAPI\\src\\JavaAPI\\AIAssist\\com\\transcribe\\AIAssist\\vocal-tracker-390118-fedb03bbaa30.json");
+		System.out.println(file.exists());
+	}
+}
